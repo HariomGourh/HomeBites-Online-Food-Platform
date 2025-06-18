527573c2e8235970a11df71cf8439b78a079e01b
Hariom Gourh <24cs10ha50@mitsgwl.ac.in>
2025-06-18 17:46:32 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+  src="https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js"
+  src="https://www.gstatic.com/firebasejs/9.22.0/firebase-auth-compat.js"
+  
+    const firebaseConfig = {
+      apiKey: "AIzaSyAobplP9lNayzaGpuZezlG3Tkb3yidu-Ds",         
+      authDomain: "homebites-ccdd0.firebaseapp.com",
+      databaseURL: "https://homebites-ccdd0-default-rtdb.firebaseio.com",
+      projectId: "homebites-ccdd0",
+      storageBucket: "homebites-ccdd0.appspot.com",
+      messagingSenderId: "442253549942",
+      appId: "1:442253549942:web:f782ad908297616b3f5a57"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+ 
